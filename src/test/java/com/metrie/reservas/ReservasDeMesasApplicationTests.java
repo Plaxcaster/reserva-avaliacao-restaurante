38b0ca35efa0420e0d7b5e7938578de072a85b07
Harryalerta <harryalerta@gmail.com>
2024-09-27 18:21:46 -0300

    Inclusão de template gerado pelo spring initializer. Foram utilizadas as mesmas dependencias utilizadas no Primeiro vídeo da segunda aula de qualidade,

---
+++ src/test/java/com/metrie/reservas/ReservasDeMesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrie.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasDeMesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
